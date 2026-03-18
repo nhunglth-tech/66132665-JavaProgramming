ed9a74b324ba9701fbd4a0a15472b76ebb289ac2
nhunglth-tech <nhung.lth.66cntt@ntu.edu.vn>
2026-03-18 20:41:07 +0700

    In kết quả tính toán

--- CacPhepToan/src/Bai3_Cacpheptoan.java
+++ CacPhepToan/src/Bai3_Cacpheptoan.java
@@ -3,7 +3,7 @@ public class Bai3_Cacpheptoan {
 
 	public static void main(String[] args) {
 int a = 3, b = 4;
-		
+		//
 		System.out.println("Tong " +a+" + " +b+ " = " +(a+b));
 		System.out.println("Hieu " +a+ " - " +b+ " = " +(a-b));
 		System.out.println("Tich " +a+ " x " +b+ " = " +(a*b));
