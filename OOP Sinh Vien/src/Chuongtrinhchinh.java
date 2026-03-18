9f276bc2396edae58aa83cd94d636f1bd53be240
nhunglth-tech <nhung.lth.66cntt@ntu.edu.vn>
2026-03-18 20:45:58 +0700

    Tạo đối tượng sinh viên mới

--- OOP Sinh Vien/src/Chuongtrinhchinh.java
+++ OOP Sinh Vien/src/Chuongtrinhchinh.java
@@ -1,6 +1,6 @@
 public class Chuongtrinhchinh {
     public static void main(String[] args) {
-        // Tạo mới 1 sinh viên
+        // Tạo mới 1 sinh viê
         Sinhvien sv1 = new Sinhvien();
         // cấp/gán dữ liệu cụ thể
         sv1.mssv="66112233";
