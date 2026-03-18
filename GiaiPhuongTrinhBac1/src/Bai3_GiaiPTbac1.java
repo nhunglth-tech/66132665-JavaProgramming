c099cc82a8b54cbf27b941eafcb0fd631656c35b
nhunglth-tech <nhung.lth.66cntt@ntu.edu.vn>
2026-03-18 20:42:48 +0700

    Nhập a và b

--- GiaiPhuongTrinhBac1/src/Bai3_GiaiPTbac1.java
+++ GiaiPhuongTrinhBac1/src/Bai3_GiaiPTbac1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Bai3_GiaiPTbac1 {
 	 public static void main(String[] args) {
 	        Scanner sc = new Scanner(System.in);
-
+//
 	        System.out.print("Nhap a: ");
 	        double a = sc.nextDouble();
 
