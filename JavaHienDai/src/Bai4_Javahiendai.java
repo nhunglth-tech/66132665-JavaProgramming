e88559d2e509395c52d2124a293f2a061774d4e1
nhunglth-tech <nhung.lth.66cntt@ntu.edu.vn>
2026-03-18 20:41:58 +0700

    In các dòng chữ

--- JavaHienDai/src/Bai4_Javahiendai.java
+++ JavaHienDai/src/Bai4_Javahiendai.java
@@ -6,7 +6,7 @@ public class Bai4_Javahiendai {
         var message = "Hello, World with var!";
         var number = 42;
         var list = java.util.List.of("Java", "Modern", "Features");
-        
+        //
         System.out.println(message);
         System.out.println("Number: " + number);
         System.out.println("List: " + list);
