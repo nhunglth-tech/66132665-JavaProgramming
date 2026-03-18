831b85b0b0f01e9f76e227e3fab54b731e2165ab
nhunglth-tech <nhung.lth.66cntt@ntu.edu.vn>
2026-03-18 20:47:37 +0700

    Tạo main sinh viên

--- SinhVienConstructor/src/MainSinhVien.java
+++ SinhVienConstructor/src/MainSinhVien.java
@@ -7,7 +7,7 @@ public class MainSinhVien {
         sv1.setMaSV("SV001");
         sv1.setDiemTB(8.5);
         sv1.hienThiThongTin();
-
+//
         System.out.println("\n---\n");
         // Tạo sinh viên với constructor có tham số
         SinhVien sv2 = new SinhVien("Trần Thị B", 21, "SV002");
